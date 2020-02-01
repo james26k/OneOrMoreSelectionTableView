8115c18d0a6a086f7f5242ee7e508d32844b25ef
hayakohei <k.hayashi@amazia.co.jp>
2020-02-01 12:37:14 +0900

    再設定

--- SingleAndMultipleSelectionTableView/VIewModule/SimpleAndMultipleSelectionTableViewController.swift
+++ SingleAndMultipleSelectionTableView/VIewModule/SimpleAndMultipleSelectionTableViewController.swift
@@ -12,11 +12,11 @@ import UIKit
 class SimpleAndMultipleSelectionTableViewController: UIViewController {
     private let tableView = UITableView()
 
-    private let sections = ["銭湯", "サウナ", "水風呂", "hoge"]
+    private let sections = ["お好きな銭湯", "サウナ温度", "水風呂温度", "hoge"]
     private let cellTitles = [
-        ["光明泉", "文化浴泉", "北欧", "八幡湯"],
-        ["81~85", "86~90", "91~95", "96~100", ""],
-        ["25~21", "20~18", "17~15", ""],
+        ["光明泉", "文化浴泉", "北欧", "八幡湯", "かるまる"],
+        ["81~85", "86~90", "91~95", "96~100", "100度越えがええ"],
+        ["25~21", "20~18", "17~15", "かるまるのサンダートルネードくらいくキンキンで"],
         ["hoge", "hoge", "hoge", "hoge", "hoge",
          "hoge", "hoge", "hoge", "hoge", "hoge",
          "hoge", "hoge", "hoge", "hoge", "hoge"]
@@ -26,6 +26,7 @@ class SimpleAndMultipleSelectionTableViewController: UIViewController {
         super.viewDidLoad()
         tableView.backgroundColor = .secondarySystemBackground
         tableView.tableFooterView = UIView()
+        // UITableView全体は複数選択可能に設定
         tableView.allowsMultipleSelection = true
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.dataSource = self
@@ -50,52 +51,27 @@ extension SimpleAndMultipleSelectionTableViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = CustomHeaderView()
-        headerView.setup(title: sections[section])
+        headerView.setup(headerTitle: sections[section])
         return headerView
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath)
         cell.textLabel?.text = cellTitles[indexPath.section][indexPath.row]
+        cell.selectionStyle = .none
         return cell
     }
 }
 
 extension SimpleAndMultipleSelectionTableViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-        if sections[indexPath.section] == "銭湯" {
-            return nil
-        } else {
-            return indexPath
-        }
+    func tableView(_ tableView: UITableView, willDeselectRowAt indexPath: IndexPath) -> IndexPath? {
+        return sections[indexPath.section] == "銭湯" ? nil : indexPath
     }
 
 }
 
-final class CustomHeaderView: UIView {
-    private let titleLabel = UILabel()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        titleLabel.tintColor = .label
-        titleLabel.font = .boldSystemFont(ofSize: 14)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(titleLabel)
-        titleLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
-        titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5).isActive = true
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func setup(title: String) {
-        titleLabel.text = title
-    }
-}
-
-final class CustomCell: UITableViewCell {
+private final class CustomCell: UITableViewCell {
+    // Cellが生成されるタイミングでselectedの状態がtrueならチェックを付ける
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         accessoryType = selected ? .checkmark : .none
